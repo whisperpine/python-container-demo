45513fe6da3b13e55543e9c0156420335c99afd9
Yusong <yusong.lai@icloud.com>
2024-12-03 19:27:52 +0000

    feat: use waitress as the WSGI server

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=5000)
