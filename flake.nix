whisperpine/python-container-demo

commit 52fc9c9f36962f555c40c0dfd1af1ec0d01fde43

build: simplify nix develop environment

flake.nix
@@ -20,15 +20,14 @@
         { pkgs }:
         {
           default = pkgs.mkShell {
-            venvDir = ".venv";
-            packages =
-              with pkgs;
-              [ python312 ]
-              ++ (with pkgs.python312Packages; [
-                venvShellHook
-                docker-compose
-                uv
-              ]);
+            packages = with pkgs; [
+              docker-compose
+              python313
+              uv
+            ];
+            shellHook = ''
+              uv sync
+            '';
           };
         }
       );

.python-version
@@ -1,1 +1,0 @@
-3.12

app.py
@@ -10,7 +10,11 @@ def hello() -> str:
     return "Hello, World!"
 
 
-if __name__ == "__main__":
+def main() -> None:
     from waitress import serve
 
     serve(app, host="0.0.0.0", port=5000)
+
+
+if __name__ == "__main__":
+    main()

build.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Set the name and tag for later usage.
-CONTAINER_NAME_TAG="python-container-demo:latest"
+container_name_tag="python-container-demo:latest"
 
 # Build the container image.
 echo ":: building container image..."
-docker build -t $CONTAINER_NAME_TAG .
+docker build -t $container_name_tag .
 
 echo
 # List container images with given name.
-docker images $CONTAINER_NAME_TAG
+docker images $container_name_tag
 echo

commit cae7fab2db33f07317b0566c770f3bce39311e24

fix(nix): venv shellHook in flake.nix
This issue was introduced in 52fc9c9.
Soluton: add "source .venv/bin/activate" in shellHook.

flake.nix
@@ -1,10 +1,8 @@
 {
   description = "A Nix-flake-based Python development environment";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs =
-    { self, nixpkgs }:
+    inputs:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -13,7 +11,10 @@
         "aarch64-darwin"
       ];
       forEachSupportedSystem =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+        );
     in
     {
       devShells = forEachSupportedSystem (
@@ -27,6 +28,7 @@
             ];
             shellHook = ''
               uv sync
+              source .venv/bin/activate
             '';
           };
         }